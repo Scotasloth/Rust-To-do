5fcd4447e9a18ebbed78d34783bce289f07c639d
Scotasloth <rossmcc76@gmail.com>
2024-04-07 23:04:18 +0100

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Choose Option");
+    println!("1 - Add to list");
+    println!("2 - Remove from list");
+    println!("3 - Mark as completed");
+    println!("4 - Show list");
+
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 }
 
-fn add() {
+//fn add() {
 
-}
+//}
 
-fn remove(){
+//fn remove(){
 
-}
+//}
 
-fn complete(){
+//fn complete(){
 
-}
+//}
