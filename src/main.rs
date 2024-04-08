15730166d54c0449a306bd97ef633947d2181e12
Scotasloth <rossmcc76@gmail.com>
2024-04-08 02:04:08 +0100

    Now creates list.txt if it does not exist already

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use std::env;
+use std::fs::{File, OpenOptions};
+use std::io::prelude::*;
+use std::path::Path; 
 
 fn main() {
 
-    check_file();
-
+    match check_file() {
+        Ok(file) => {
+            // File was successfully opened or created
+            // Do something with the file here
+        }
+        Err(err) => {
+            // Handle the error
+            eprintln!("Error: {}", err);
+            // Optionally, you can panic or return early from the main function
+            // panic!("Failed to open or create file");
+            // return;
+        }
+    }
+        
     println!("Choose Option");
     println!("1 - Add to list");
     println!("2 - Remove from list");
@@ -13,8 +28,6 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
     let input = args[1].clone();
-
-    println!("{:?}", input);
 
     if input =="1"{
         add()
@@ -29,10 +42,26 @@ fn main() {
     }
 }
 
-fn check_file() {
-    let dir = r"C:\Users\Ross\OneDrive\Documents\Programs\Rust\Misc\todo>";
+fn check_file() -> Result<File, std::io::Error> {
+    let file_path = "C:/Users/Ross/OneDrive/Documents/Programs/Rust/Misc/todo/src/list.txt";
 
+    let exists = Path::new(&file_path).exists();
+
+    if !exists {
+        println!("File not found! Creating new file...");
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(&file_path)?;
+        Ok(file)
+    } else {
+        println!("File found!");
+        let file = File::open(&file_path)?;
+        Ok(file)
+    }
 }
+
+
 
 fn add() {
     println!("adding")
